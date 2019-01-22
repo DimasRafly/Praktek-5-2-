8ed1be6ae44728c9cd2adb5afd60802a4a5680b9
Your Name <you@example.com>
2019-01-22 14:05:20 +0700

    selesai

---
+++ application/views/contact.php
@@ -0,0 +1,34 @@
+<div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>
+                                CONTACT PERSON
+                            </h2>
+                            <ul class="header-dropdown m-r--5">
+                                <li class="dropdown">
+                                    <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                        <i class="material-icons">more_vert</i>
+                                    </a>
+                                    <ul class="dropdown-menu pull-right">
+                                        <li><a href="javascript:void(0);">Action</a></li>
+                                        <li><a href="javascript:void(0);">Another action</a></li>
+                                        <li><a href="javascript:void(0);">Something else here</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="body">
+                            <p class="lead">
+                            	<img style="width: 10%" src="<?=base_url('assets/images/dimas.jpg')?>">
+                                Dimas Rafly Abimanyu
+                            </p>
+                            	<i class="material-icons">phone</i>	+6285706080186	<br>
+                            	<i class="material-icons">email</i>	dimasrafly17@gmail.com	<br>
+                            	<i class="material-icons">turned_in</i> dimasraflyy_	<br>
+                            	<i class="material-icons">thumb_up_alt</i> Dimas Rafly Jr	<br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
---
+++ application/views/gallery.php
@@ -0,0 +1,127 @@
+<div class="gallery">
+        <h2 class="w3ls_head" style="text-align: center;">Gallery</h2><br>
+        <div class="gallery-grids">
+                <div class="gallery-top-grids">
+                    <div class="col-sm-4 gallery-grids-left">
+                        <div class="gallery-grid">
+                            <a class="example-image-link" href="<?=base_url('assets/images/yeti.png')?>" data-lightbox="example-set" data-title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae cursus ligula">
+                                <img src="<?=base_url('assets/images/yeti.png')?>" style="width: 50%" alt="" />
+                                <div class="captn">
+                                    <h4>Yeti</h4>
+                                    <p>SB130 TURQ X01 2019</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 gallery-grids-left">
+                        <div class="gallery-grid">
+                            <a class="example-image-link" href="<?=base_url('assets/images/felt.png')?>" data-lightbox="example-set" data-title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae cursus ligula">
+                                <img src="<?=base_url('assets/images/felt.png')?>" style="width: 50%" alt="" />
+                                <div class="captn">
+                                    <h4>Felt</h4>
+                                    <p>Doctrine 5 NX EAGLE</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 gallery-grids-left">
+                        <div class="gallery-grid">
+                            <a class="example-image-link" href="<?=base_url('assets/images/gt.png')?>" data-lightbox="example-set" data-title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae cursus ligula">
+                                <img src="<?=base_url('assets/images/gt.png')?>" style="width: 50%" alt="" />
+                                <div class="captn">
+                                    <h4>GT</h4>
+                                    <p>Force Carbon Expert</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="gallery-top-grids">
+                    <div class="col-sm-4 gallery-grids-left">
+                        <div class="gallery-grid">
+                            <a class="example-image-link" href="<?=base_url('assets/images/1.jpng')?>" data-lightbox="example-set" data-title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae cursus ligula">
+                                <img src="<?=base_url('assets/images/1.png')?>" style="width: 50%" alt="" />
+                                <div class="captn">
+                                    <h4>Ibis</h4>
+                                    <p>Ripley LS NX 2018</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 gallery-grids-left">
+                        <div class="gallery-grid">
+                            <a class="example-image-link" href="<?=base_url('assets/images/trek.png')?>" data-lightbox="example-set" data-title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae cursus ligula">
+                                <img src="<?=base_url('assets/images/trek.png')?>" style="width: 50%" alt="" />
+                                <div class="captn">
+                                    <h4>Trek</h4>
+                                    <p>Powerfly FS 7 Plus</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 gallery-grids-left">
+                        <div class="gallery-grid">
+                            <a class="example-image-link" href="<?=base_url('assets/images/2.png')?>" data-lightbox="example-set" data-title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae cursus ligula">
+                                <img src="<?=base_url('assets/images/2.png')?>" style="width: 50%" alt="" />
+                                <div class="captn">
+                                    <h4>Giant</h4>
+                                    <p>Anthem Advanced Pro 29</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="gallery-top-grids">
+                    <div class="col-sm-4 gallery-grids-left">
+                        <div class="gallery-grid">
+                            <a class="example-image-link" href="<?=base_url('assets/images/3.png')?>" data-lightbox="example-set" data-title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae cursus ligula">
+                                <img src="<?=base_url('assets/images/3.png')?>" style="width: 50%" alt="" />
+                                <div class="captn">
+                                    <h4>Specialized</h4>
+                                    <p>Demo 8 Carbon</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 gallery-grids-left">
+                        <div class="gallery-grid">
+                            <a class="example-image-link" href="<?=base_url('assets/images/4.png')?>" data-lightbox="example-set" data-title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae cursus ligula">
+                                <img src="<?=base_url('assets/images/4.png')?>" style="width: 50%" alt="" />
+                                <div class="captn">
+                                    <h4>Santa Cruz</h4>
+                                    <p>The New V10</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 gallery-grids-left">
+                        <div class="gallery-grid">
+                            <a class="example-image-link" href="<?=base_url('assets/images/5.png')?>" data-lightbox="example-set" data-title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae cursus ligula">
+                                <img src="<?=base_url('assets/images/5.png')?>" style="width: 50%" alt="" />
+                                <div class="captn">
+                                    <h4>Cannondale</h4>
+                                    <p>Triger 3 2018</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="clearfix"> </div>
+                <script src="js/lightbox-plus-jquery.min.js"> </script>
+        
+    </div>
+    </div>
+    
+
+</section>
+ 
+          <div class="footer">
+            <div class="wthree-copyright">
+              
+            </div>
+          </div>
